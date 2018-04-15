b9cc43ff54eac02756f4900f7006d5faf84d208c
Robin <499549060@qq.com>
2018-04-15 20:40:50 +0800

    1

--- Demo4/Q1/app/src/main/java/com/example/administrator/myapplication/MainActivity.java
+++ Demo4/Q1/app/src/main/java/com/example/administrator/myapplication/MainActivity.java
@@ -1,44 +1,42 @@
 package com.example.administrator.myapplication;
-
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.widget.SimpleAdapter;
-import android.widget.Spinner;
-
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
-import java.util.PriorityQueue;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    private Spinner spinner;
-    private SimpleAdapter adapter;
-    private List<Map<String,Object>> list;
-
+    private String[] names = new String[] {"Lion", "Tiger", "Monkey","Dog","Cat","Elephant"};
+    private int[] imageIds = new int[] {R.drawable.lion, R.drawable.tiger,R.drawable.monkey,R.drawable.dog,R.drawable.cat,R.drawable.elephant};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //t通过资源ID获得控件
-        spinner = (Spinner ) findViewById(R.id.spinner);
-        //获得数据源
-        getData();
-        adapter = new SimpleAdapter(
-                this,
-                list,
-                R.layout.item,
-                new String[]{"Icon", "name"},
-                new int[] {R.id.Img, R.id.Text}
-        );
-        //适配器与控件绑定
-        spinner.setAdapter(adapter);
-        spinner.setOnItemClickListener(this);
-    }
-
-    private void getData() {
-        list = new ArrayList<Map<String,Object>>();
-        Map map = new HashMap<String, Object>();
-        map.put("name", "")
+        List<Map<String, Object>> listItems = new ArrayList<Map<String, Object>>();
+        for (int i = 0; i < names.length; i++) {
+            Map<String, Object> listItem = new HashMap<String, Object>();
+            listItem.put("header", imageIds[i]);
+            listItem.put("Name", names[i]);
+            listItems.add(listItem);
+        }
+        SimpleAdapter simpleadapter = new SimpleAdapter(this, listItems, R.layout.activity_main,new String[] {"Name", "header"}, new int[] {R.id.name,R.id.header});
+        ListView list = (ListView) findViewById(R.id.mylist);
+        list.setAdapter(simpleadapter);
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast toast = Toast.makeText(MainActivity.this , names[position], Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        });
     }
 }
